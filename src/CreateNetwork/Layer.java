878c777a700a8f1e9f1469bd53fee7dbb302dcf1
= <lukasbajgar2007@gmail.com>
2024-04-26 17:10:49 +0200

    set biases, weights for layer

--- src/CreateNetwork/Layer.java
+++ src/CreateNetwork/Layer.java
@@ -1,4 +1,5 @@
 package CreateNetwork;
+
 public class Layer {
     int numNodesIn, numNodesOut;
     double[][] weights;
@@ -10,6 +11,22 @@ public class Layer {
 
         weights = new double[numNodesIn][numNodesOut];
         biases = new double[numNodesOut];
+    }
+
+    public void setBiases(double[] biasesIn) {
+
+        for (int i = 0; i < numNodesOut; i++) {
+            biases[i] = biasesIn[i];
+        }
+
+    }
+
+    public void setWeights(double[][] weightsIn) {
+        for (int i = 0; i < numNodesIn; i++) {
+            for (int j = 0; j < numNodesOut; j++) {
+                weights[i][j] = weightsIn[i][j];
+            }
+        }
     }
 
     public double[] calculateOutputs(double[] inputs) {
